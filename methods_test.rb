7947334e4ec37751591cc4834037e1ef5ad5596d
Nick Rank <ranknc@s.dcsdk12.org>
2015-08-26 11:02:53 -0600

    w

--- methods_test.rb
+++ methods_test.rb
@@ -38,4 +38,13 @@ class MethodsTest < MiniTest::Unit::TestCase
   	assert_equal -16, @m.sum_double(-4, -4)
   end
 
+  def test_diff21
+    assert_equal 11, @m.diff21(10)
+    assert_equal 21, @m.diff21(0)
+    assert_equal 0, @m.diff21(21)
+    assert_equal 2, @m.diff21(22)
+    assert_equal 44, @m.diff21(43) 
+
+  end
+
 end
